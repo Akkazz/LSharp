bf9c8369d243cf493a1add2167309cce87577ea9
Akkazz <akkazz01@gmail.com>
2016-07-10 01:36:29 -0300

    TargettedSpells

--- Firestorm AIO/Firestorm AIO/Ultilities/Activator/ActivatorEvents.cs
+++ Firestorm AIO/Firestorm AIO/Ultilities/Activator/ActivatorEvents.cs
@@ -9,6 +9,7 @@ using Firestorm_AIO.Enums;
 using Firestorm_AIO.Helpers;
 using LeagueSharp;
 using LeagueSharp.SDK;
+using LeagueSharp.SDK.Utils;
 using static Firestorm_AIO.Helpers.Helpers;
 
 namespace Firestorm_AIO.Ultilities.Activator
@@ -17,9 +18,24 @@ namespace Firestorm_AIO.Ultilities.Activator
     {
         #region EventsStuff
 
+        public class OnDangerEventArgs : EventArgs
+        {
+            public int DangerLevel { get; private set; }
+            public SpellSlot Slot { get; private set; }
+            public Champion Champion { get; private set; }
+
+            public OnDangerEventArgs(int dangerLevel, SpellSlot slot, Champion champ)
+            {
+                DangerLevel = dangerLevel;
+                Slot = slot;
+                Champion = champ;
+            }
+        }
+
         public delegate void OnDanderEventHandler(Obj_AI_Base sender, OnDangerEventArgs args);
 
         public static event OnDanderEventHandler OnDanger;
+
         #endregion EventsStuff
 
         public static void Load()
@@ -61,12 +77,23 @@ namespace Firestorm_AIO.Ultilities.Activator
             {
                 var missileInfo = hero.GetMissileInfo();
 
-                if(missileInfo == null)return;
+                if (missileInfo != null)
+                {
+                    OnDanger.Invoke(hero, missileInfo);
+                }
+                else
+                {
+                    var targettedInfo = TargetteSpells.First();
+                    if (targettedInfo.Target != null)
+                    {
+                        
+                    }
+                }
 
-                OnDanger.Invoke(hero, missileInfo);
+
             }
         }
-        
+
         #region Extensions
 
         private static OnDangerEventArgs GetMissileInfo(this Obj_AI_Base target)
@@ -74,7 +101,6 @@ namespace Firestorm_AIO.Ultilities.Activator
             //Missiles
             var missile = Missiles.FirstOrDefault(m => m.IsInRange(target, 3000) && m.IsValid);
 
-            var sliderPercent = 50;
             var boundingRadius = target.BoundingRadius + 80;
 
             var champion = missile?.SpellCaster as Obj_AI_Hero;
@@ -84,8 +110,9 @@ namespace Firestorm_AIO.Ultilities.Activator
                 var projection = target.Position
                     .ProjectOn(missile.StartPosition, missile.EndPosition);
 
-                if (projection.IsOnSegment &&
-                    projection.SegmentPoint.Distance(target.Position) <= missile.SData.CastRadius + boundingRadius)
+                if ((projection.IsOnSegment &&
+                     projection.SegmentPoint.Distance(target.Position) <= missile.SData.CastRadius + boundingRadius) ||
+                    missile.Target.IsMe)
                 {
                     var dangerLevel = 0;
 
@@ -121,7 +148,7 @@ namespace Firestorm_AIO.Ultilities.Activator
                             break;
                     }
 
-                    if (dangerLevel != 0 && target.HealthPercent <= sliderPercent)
+                    if (dangerLevel != 0)
                     {
                         return new OnDangerEventArgs(dangerLevel, missile.Slot, champion.GetChampion());
                     }
@@ -132,10 +159,67 @@ namespace Firestorm_AIO.Ultilities.Activator
 
         #endregion Extensions
 
+        private static List<TargetteSpell> TargetteSpells = new List<TargetteSpell>();
+
+        private struct TargetteSpell
+        {
+            public Obj_AI_Hero Target;
+            public Obj_AI_Hero Sender;
+            public int DangerLevel;
+        }
+
         private static void Obj_AI_Base_OnProcessSpellCast(Obj_AI_Base sender, GameObjectProcessSpellCastEventArgs args)
         {
-            if (sender == null || OnDanger == null || Me.IsDead) return;
+            var champion = sender as Obj_AI_Hero;
+            var target = args.Target as Obj_AI_Hero;
+
+            if (champion == null || target == null || target.IsAlly || OnDanger == null) return;
+
+            var dangerLevel = 0;
+
+            switch (args.Slot)
+            {
+                case SpellSlot.Q:
+                    var qDangerLevel =
+                        SpellDanger.SpellDangerDB.FirstOrDefault(
+                            dl => dl.QDangerLevel != 0 && dl.Champ == champion.GetChampion());
+
+                    if (qDangerLevel != null) dangerLevel = qDangerLevel.QDangerLevel;
+                    break;
+                case SpellSlot.W:
+                    var wDangerLevel =
+                        SpellDanger.SpellDangerDB.FirstOrDefault(
+                            dl => dl.WDangerLevel != 0 && dl.Champ == champion.GetChampion());
+
+                    if (wDangerLevel != null) dangerLevel = wDangerLevel.WDangerLevel;
+                    break;
+                case SpellSlot.E:
+                    var eDangerLevel =
+                        SpellDanger.SpellDangerDB.FirstOrDefault(
+                            dl => dl.EDangerLevel != 0 && dl.Champ == champion.GetChampion());
+
+                    if (eDangerLevel != null) dangerLevel = eDangerLevel.EDangerLevel;
+                    break;
+                case SpellSlot.R:
+                    var rDangerLevel =
+                        SpellDanger.SpellDangerDB.FirstOrDefault(
+                            dl => dl.RDangerLevel != 0 && dl.Champ == champion.GetChampion());
+
+                    if (rDangerLevel != null) dangerLevel = rDangerLevel.RDangerLevel;
+                    break;
+            }
+
+            
+            if (dangerLevel != 0)
+            {
+                var targSpell = new TargetteSpell {DangerLevel = dangerLevel, Sender = champion, Target = target};
+
+                TargetteSpells.Add(targSpell);
+                DelayAction.Add(args.Start.Distance(args.End) / args.SData.MissileSpeed, () => TargetteSpells.Remove(targSpell));
+            }
         }
+
+
 
         #region Missiles Stuff
 
@@ -144,7 +228,7 @@ namespace Firestorm_AIO.Ultilities.Activator
         private static void GameObject_OnCreate(GameObject sender, EventArgs args)
         {
             var missile = sender as MissileClient;
-            if (missile == null || OnDanger == null || Me.IsDead || sender.IsAlly) return;
+            if (missile == null || OnDanger == null || sender.IsAlly) return;
 
             var senderHero = missile.SpellCaster as Obj_AI_Hero;
             if (senderHero == null) return;
@@ -164,19 +248,5 @@ namespace Firestorm_AIO.Ultilities.Activator
         }
 
         #endregion Missiles Stuff
-    }
-
-    public class OnDangerEventArgs : EventArgs
-    {
-        public int DangerLevel { get; private set; }
-        public SpellSlot Slot { get; private set; }
-        public Champion Champion { get; private set; }
-
-        public OnDangerEventArgs(int dangerLevel, SpellSlot slot, Champion champ)
-        {
-            DangerLevel = dangerLevel;
-            Slot = slot;
-            Champion = champ;
-        }
     }
 }
